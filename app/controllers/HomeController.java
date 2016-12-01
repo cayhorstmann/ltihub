dc79c4d1d9dbf29edddb07e408ce2a4cb336d2f9
Sunita Rajain <work.sunita@gmail.com>
2016-12-01 21:10:48 +0000

    HomeController.java edited online with Bitbucket

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -27,15 +27,12 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 	 
-    public Result index() {
-String url = controllers.routes.HomeController.getAssignment().url()
-                + "?id=" + request().getQueryString("id");
-        Logger.info(url);
-        return redirect(url);      
- // return redirect(routes.HomeController.getAssignment());
-//  return ok("Done");
+    public Result index() {     
+ 		 return redirect(routes.HomeController.getAssignment());
+		//return ok("Done");
   }
-	
+  
+	//Method to show the assignment landing page
 	public Result createAssignment() {
         Map<String, String[]> postParams = request().body().asFormUrlEncoded();
         response().setCookie(new Http.Cookie("launch_presentation_return_url", postParams.get("launch_presentation_return_url")[0],
@@ -44,79 +41,69 @@ String url = controllers.routes.HomeController.getAssignment().url()
         return ok(create_exercise.render());
     }
 	
+	//Method to save the created assignment
 	public Result addAssignment() {        
-	DynamicForm bindedForm = Form.form().bindFromRequest();
-        String problemlist = bindedForm.get("url");
-	Assignment assignment = new Assignment();
-	assignment.save();
-        System.out.println(problemlist);
-	if(null != problemlist|| !problemlist.equals("")) {
-		String [] problemArr = problemlist.split(","); 
-		for(String problemstr : problemArr) {
-			if(null != problemstr && !problemstr.equals("")) {
-				Problem problem = new Problem();
-				problem.setProblemUrl(problemstr);
-				problem.setAssignment(assignment);
-				assignment.getProblems().add(problem);
-				problem.save();
-			}	
-		}
+		DynamicForm bindedForm = Form.form().bindFromRequest();
+   		String problemlist = bindedForm.get("url");
+		Assignment assignment = new Assignment();
 		assignment.save();
-		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
-		System.out.println(problems);
-	}
-	return redirect(routes.HomeController.showAssignment(assignment.getAssignmentId()));
-}
-	
-public Result getAssignment(){
-	Long assignmentId = Long.parseLong(request().getQueryString("id"));
-	Assignment assignment = Assignment.find.byId(assignmentId);
-	List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
-        System.out.println(problems);
-        return ok(finalAssignment.render(problems));
-}	
-
-public Result saveEditedAssignment(Long assignment) {
-        
-	DynamicForm bindedForm = Form.form().bindFromRequest();
-        String problemlist = bindedForm.get("url");
-	Assignment assignment1 = Assignment.find.byId(assignment);
         System.out.println(problemlist);
-	if(null != problemlist|| !problemlist.equals("")) {
-		String [] problemArr = problemlist.split(","); 
-		for(String problemstr : problemArr) {
-			if(null != problemstr && !problemstr.equals("")) {
-				Problem problem = new Problem();
-				problem.setProblemUrl(problemstr);
-				problem.setAssignment(assignment1);
-				assignment1.getProblems().add(problem);
-				problem.save();
-			}	
+		if(null != problemlist|| !problemlist.equals("")) {
+			String [] problemArr = problemlist.split(","); 
+			for(String problemstr : problemArr) {
+				if(null != problemstr && !problemstr.equals("")) {
+					Problem problem = new Problem();
+					problem.setProblemUrl(problemstr);
+					problem.setAssignment(assignment);
+					assignment.getProblems().add(problem);
+					problem.save();
+				}	
+			}
+		
 		}
-		assignment1.save();
+		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
+	
+		Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
+		String returnUrl = launchReturnUrlCookie.value();
+		System.out.println("ReturnURL is: " + returnUrl);
+	
+		return ok(showassignment.render(returnUrl,assignment1,problems));
 	}
-	List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
+	
+	public Result getAssignment(){
+		Long assignmentId = Long.parseLong(request().getQueryString("id"));
+		Assignment assignment = Assignment.find.byId(assignmentId);
+		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
         System.out.println(problems);
         return ok(finalAssignment.render(problems));
-	}
-    /**
-     * GET method
-     */
-    public Result showAssignment(Long assignment) {
-		
-	Assignment assignment1 = Assignment.find.byId(assignment);
-	System.out.println(assignment1.getAssignmentId());
-        List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
-	Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
+	}	
 
-	String returnUrl = launchReturnUrlCookie.value();
-	System.out.println("ReturnURL is: " + returnUrl);
-        System.out.println(problems);
-        return ok(showassignment.render(returnUrl,assignment1,problems));    
-    }
+	public Result saveEditedAssignment(Long assignment) {
+        
+		DynamicForm bindedForm = Form.form().bindFromRequest();
+        String problemlist = bindedForm.get("url");
+		Assignment assignment1 = Assignment.find.byId(assignment);
+        System.out.println(problemlist);
+		if(null != problemlist|| !problemlist.equals("")) {
+			String [] problemArr = problemlist.split(","); 
+			for(String problemstr : problemArr) {
+				if(null != problemstr && !problemstr.equals("")) {
+					Problem problem = new Problem();
+					problem.setProblemUrl(problemstr);
+					problem.setAssignment(assignment1);
+					assignment1.getProblems().add(problem);
+					problem.save();
+				}	
+			}
+		assignment1.save();
+		}
+		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
+    	System.out.println(problems);
+        return ok(showassignment.render(returnUrl,assignment1,problems));
+	}
+    
 
 	public Result showEditPage(Long assignment) {
-		Map<String, String[]> postParams = request().body().asFormUrlEncoded();
 		Assignment assignment1 = Assignment.find.byId(assignment);
 		System.out.println(assignment1.getAssignmentId());
         List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
