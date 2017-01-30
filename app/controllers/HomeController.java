e962b0b62c77e70d0c91a08f1e0c32f0e871ef03
Cay Horstmann <cay@horstmann.com>
2017-01-30 07:00:01 -0800

    S.o.p to Logger.info

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -26,23 +26,23 @@ public class HomeController extends Controller {
     public Result index() throws UnsupportedEncodingException{    
  	Map<String, String[]> postParams = request().body().asFormUrlEncoded();
     	for(String key: postParams.keySet())
-    		System.out.println(key + " - " + Arrays.toString(postParams.get(key)));
+    		Logger.info(key + " - " + Arrays.toString(postParams.get(key)));
 	
 	if (postParams.get("lis_outcome_service_url") == null || postParams.get("lis_result_sourcedid") == null) {
           	flash("warning", "");
 	}
 	else{
-		System.out.println("OutconeService URL is: " + postParams.get("lis_outcome_service_url")[0]);
+		Logger.info("OutconeService URL is: " + postParams.get("lis_outcome_service_url")[0]);
 		response().setCookie(new Http.Cookie("lis_outcome_service_url", postParams.get("lis_outcome_service_url")[0],
                  null, null, null, false, false));
-		System.out.println("Result sourcedId is: " +postParams.get("lis_result_sourcedid")[0]);
+		Logger.info("Result sourcedId is: " +postParams.get("lis_result_sourcedid")[0]);
 	response().setCookie(new Http.Cookie("lis_result_sourcedid", URLEncoder.encode(postParams.get("lis_result_sourcedid")[0],"UTF-8"),
                  null, null, null, false, false));
 	}
 
 if(postParams.get("custom_canvas_user_id")==null){
 	String userid = postParams.get("user_id")[0];
-	System.out.println("User ID from ilearn is: " + userid);
+	Logger.info("User ID from ilearn is: " + userid);
 	response().setCookie(new Http.Cookie("custom_canvas_user_id", postParams.get("user_id")[0],
                   null, null, null, false, false));
 }else
@@ -56,12 +56,12 @@ if(postParams.get("custom_canvas_user_id")==null){
   
 	//Method to show the assignment landing page
 	public Result createAssignment() {
-		System.out.println("Parameters received from canvas in instructor view:create assignment");
+		Logger.info("Parameters received from canvas in instructor view:create assignment");
         Map<String, String[]> postParams = request().body().asFormUrlEncoded();
 
 	for(String key: postParams.keySet())
-    		System.out.println(key + " - " + Arrays.toString(postParams.get(key)));
-	 System.out.println();
+    		Logger.info(key + " - " + Arrays.toString(postParams.get(key)));
+	 Logger.info();
          response().setCookie(new Http.Cookie("launch_presentation_return_url", postParams.get("launch_presentation_return_url")[0],
                     null, null, null, false, false));
          return ok(create_exercise.render());
@@ -73,7 +73,7 @@ if(postParams.get("custom_canvas_user_id")==null){
    		String problemlist = bindedForm.get("url");
 		Assignment assignment = new Assignment();
 		assignment.save();
- 	        System.out.println(problemlist);
+ 	        Logger.info(problemlist);
 		if(null != problemlist|| !problemlist.equals("")) {
 			String [] problemArr = problemlist.split(","); 
 			for(String problemstr : problemArr) {
@@ -99,15 +99,15 @@ if(postParams.get("custom_canvas_user_id")==null){
 	     Long assignmentId = Long.parseLong(request().getQueryString("id"));
 	 
 	     List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignmentId).findList();
-	System.out.println("Inside Get Statement");
-	System.out.println("user ID value from cookie is: " + request().cookie("custom_canvas_user_id").value());
+	Logger.info("Inside Get Statement");
+	Logger.info("user ID value from cookie is: " + request().cookie("custom_canvas_user_id").value());
 	Http.Cookie userIdCookie = request().cookie("custom_canvas_user_id");
 	Long userId = Long.parseLong(userIdCookie.value());
-	System.out.println("UserID is: " + userId);
+	Logger.info("UserID is: " + userId);
 	List<Submission> submissions = new ArrayList<Submission>();
 	for(Problem problem: problems){
 	List<Submission> submissionsAll = Submission.find.where().eq("problem.problemId",problem.problemId).eq("canvasAssignmentId",assignmentId).eq("studentId",userId).findList();
-	System.out.println("Submission list is: " + submissionsAll);
+	Logger.info("Submission list is: " + submissionsAll);
 	int correctForThisProblem = 0;
 	int maxscoreForThisProblem = 0;
         if(submissionsAll.size()!=0){
@@ -120,8 +120,8 @@ if(postParams.get("custom_canvas_user_id")==null){
 	Submission submission = Submission.find.where().eq("problem.problemId", problem.problemId).eq("canvasAssignmentId", assignmentId).eq("studentId",userId).eq("correct",correctForThisProblem).findList().get(0);
 	submissions.add(submission);			
 	}}
-        System.out.println("Submission list is: " + submissions);
-	System.out.println("Problems list is: " + problems);
+        Logger.info("Submission list is: " + submissions);
+	Logger.info("Problems list is: " + problems);
         if(submissions.size()==0)
 	return ok(finalAssignment.render(problems,assignmentId, userId, getPrefix()));
 
@@ -138,7 +138,7 @@ if(postParams.get("custom_canvas_user_id")==null){
 	      DynamicForm bindedForm = Form.form().bindFromRequest();
               String problemlist = bindedForm.get("url");
 	      Assignment assignment1 = Assignment.find.byId(assignment);
-              System.out.println(problemlist);
+              Logger.info(problemlist);
 		if(null != problemlist|| !problemlist.equals("")) {
 			String [] problemArr = problemlist.split(","); 
 			for(String problemstr : problemArr) {
@@ -152,19 +152,19 @@ if(postParams.get("custom_canvas_user_id")==null){
 			}
 		}
 	     List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
-    	System.out.println(problems);
+    	Logger.info(problems);
 	    Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
 	    String returnUrl = launchReturnUrlCookie.value();
-	System.out.println("ReturnURL is: " + returnUrl);
+	Logger.info("ReturnURL is: " + returnUrl);
         return ok(showassignment.render(returnUrl,assignment1,problems));
 	}
     
 
 	public Result showEditPage(Long assignment) {
 		Assignment assignment1 = Assignment.find.byId(assignment);
-		System.out.println(assignment1.getAssignmentId());
+		Logger.info(assignment1.getAssignmentId());
         	List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
-        	System.out.println(problems);
+        	Logger.info(problems);
         	return ok(editAssignment.render(assignment1, problems));    
     }
 
