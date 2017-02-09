2b0a65afb5240ac21bf0379e4882fa5c9d2d2db9
Sunita Rajain <work.sunita@gmail.com>
2017-02-09 17:10:55 +0000

    Assignment can be created outside LMS

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -25,8 +25,7 @@ public class HomeController extends Controller {
 
     public Result config() throws UnknownHostException {
         Http.Request request = request();
-        return ok(views.xml.lti_config
-                .render())
+        return ok(views.xml.lti_config.render())
                 .as("application/xml");
     }     
     
@@ -34,25 +33,22 @@ public class HomeController extends Controller {
  	Map<String, String[]> postParams = request().body().asFormUrlEncoded();
     	for(String key: postParams.keySet())
     		Logger.info(key + " - " + Arrays.toString(postParams.get(key)));
-	
+
 	if (postParams.get("lis_outcome_service_url") == null || postParams.get("lis_result_sourcedid") == null) {
           	flash("warning", "");
 	}
-	else{
-		response().setCookie(new Http.Cookie("lis_outcome_service_url", postParams.get("lis_outcome_service_url")[0],
+	else{	
+	response().setCookie(new Http.Cookie("lis_outcome_service_url", postParams.get("lis_outcome_service_url")[0],
                  null, null, null, false, false));
-		response().setCookie(new Http.Cookie("lis_result_sourcedid", URLEncoder.encode(postParams.get("lis_result_sourcedid")[0],"UTF-8"),
+	response().setCookie(new Http.Cookie("lis_result_sourcedid", URLEncoder.encode(postParams.get("lis_result_sourcedid")[0],"UTF-8"),
                  null, null, null, false, false));
 	}
-
 	if(postParams.get("custom_canvas_user_id")==null){
 		String userid = postParams.get("user_id")[0];
 		Logger.info("User ID from ilearn is: " + userid);
-		response().setCookie(new Http.Cookie("custom_canvas_user_id", postParams.get("user_id")[0],null, null, null, false, false));
+		response().setCookie(new Http.Cookie("user_id", postParams.get("user_id")[0],null, null, null, false, false));
 	}else
-	response().setCookie(new Http.Cookie("custom_canvas_user_id", postParams.get("custom_canvas_user_id")[0],null, null, null, false, false));
-	//String url = controllers.routes.HomeController.getAssignment().url()
-          //      + "?id=" + URLEncoder.encode(request().getQueryString("id"), "UTF-8")
+		response().setCookie(new Http.Cookie("user_id", postParams.get("custom_canvas_user_id")[0],null, null, null, false, false));
 	response().setCookie(new Http.Cookie("launch_presentation_return_url", postParams.get("launch_presentation_return_url")[0],
                     null, null, null, false, false));
 	String contextID = postParams.get("context_id")[0];
@@ -66,39 +62,76 @@ public class HomeController extends Controller {
 		return ok(create_exercise.render(contextID, resourceLinkID, toolConsumerInstanceGuID));
 
 	return redirect(controllers.routes.HomeController.getAssignment(role, Long.parseLong(assignmentId)));	
-     	//return redirect(url);
+     	
  	}
-	
+
+	public Result createAssignment() {
+		
+         return ok(create_exercise_outside_LMS.render());
+    }
+		
 	//Method to save the created assignment
 	public Result addAssignment() {        
-		DynamicForm bindedForm = Form.form().bindFromRequest();
-   		String problemlist = bindedForm.get("url");
+	       DynamicForm bindedForm = Form.form().bindFromRequest();
+   	       String problemlist = bindedForm.get("url");
 	
-		Assignment assignment = new Assignment();
-		assignment.setContextId(bindedForm.get("context_id"));
-		assignment.setResourceLinkId(bindedForm.get("resource_link_id"));
-		assignment.setToolConsumerInstanceGuId(bindedForm.get("tool_consumer_id"));
-		assignment.save();
- 	        Logger.info(problemlist);
-		if(null != problemlist|| !problemlist.equals("")) {
-			String [] problemArr = problemlist.split(","); 
-			for(String problemstr : problemArr) {
-				if(null != problemstr && !problemstr.equals("")) {
-					Problem problem = new Problem();
-					problem.setProblemUrl(problemstr);
-					problem.setAssignment(assignment);
-					assignment.getProblems().add(problem);
-					problem.save();
-				}	
-			}
-		}
-		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
+               Assignment assignment = new Assignment();
+	       assignment.setContextId(bindedForm.get("context_id"));
+	       assignment.setResourceLinkId(bindedForm.get("resource_link_id"));
+	       assignment.setToolConsumerInstanceGuId(bindedForm.get("tool_consumer_id"));
+	       assignment.save();
+ 	       Logger.info(problemlist);
+	   
+               if(null != problemlist|| !problemlist.equals("")) {
+		      String [] problemArr = problemlist.split("\n"); 
+		      for(String problemstr : problemArr) {
+			  if(null != problemstr && !problemstr.equals("")) {
+				  Problem problem = new Problem();
+				  problem.setProblemUrl(problemstr);
+				  problem.setAssignment(assignment);
+				  assignment.getProblems().add(problem);
+				  problem.save();
+			  }	
+		     }
+	     }
+	     List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
 	
-		Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
-		String returnUrl = launchReturnUrlCookie.value();
+	     Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
+	     String returnUrl = launchReturnUrlCookie.value();
 	
-		return ok(showassignment.render(returnUrl,assignment,problems, getPrefix()));
+	     return ok(showassignment.render(returnUrl,assignment,problems, getPrefix()));
+
+       }
+	
+	public Result addAssignmentOutsideLMS() {        
+	      DynamicForm bindedForm = Form.form().bindFromRequest();
+   	      String problemlist = bindedForm.get("url");
+	      String key = bindedForm.get("key");
+	      String secret = bindedForm.get("secret");
+
+	      if(key.equals("fred") && secret.equals("fred")){
+	      Assignment assignment = new Assignment();
+	      assignment.save();
+
+ 	      Logger.info(problemlist);
+	      if(null != problemlist|| !problemlist.equals("")) {
+		     String [] problemArr = problemlist.split("\n"); 
+		     for(String problemstr : problemArr) {
+			  if(null != problemstr && !problemstr.equals("")) {
+				Problem problem = new Problem();
+				problem.setProblemUrl(problemstr);
+				problem.setAssignment(assignment);
+				assignment.getProblems().add(problem);
+				problem.save();
+			  }	
+		    }
+	      }
+	List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
+	return ok(showassignmentOutsideLMS.render(assignment,problems, getPrefix()));
 	}
+	else
+		return ok("Secret or key doesn't match.");
+}
 	
 	//Get Assignment Method
 	public Result getAssignment(String role, Long assignmentId){
@@ -109,13 +142,13 @@ public class HomeController extends Controller {
 	return ok(editAssignment.render(assignment, problems));
 	}
 	List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignmentId).findList();
-	Http.Cookie userIdCookie = request().cookie("custom_canvas_user_id");
+	Http.Cookie userIdCookie = request().cookie("user_id");
 	String userId = userIdCookie.value();
 	Logger.info("UserID is: " + userId);
 	List<Submission> submissions = new ArrayList<Submission>();
 	for(Problem problem: problems){
 	List<Submission> submissionsAll = Submission.find.where().eq("problem.problemId",problem.problemId).eq("assignmentId",assignmentId).eq("studentId",userId).findList();
-	Logger.info("Submission list is: " + submissionsAll);
+	
 	int correctForThisProblem = 0;
 	int maxscoreForThisProblem = 0;
         if(submissionsAll.size()!=0){
@@ -128,7 +161,7 @@ public class HomeController extends Controller {
 	Submission submission = Submission.find.where().eq("problem.problemId", problem.problemId).eq("assignmentId", assignmentId).eq("studentId",userId).eq("correct",correctForThisProblem).findList().get(0);
 	submissions.add(submission);			
 	}}
-        Logger.info("Submission list is: " + submissions);
+        Logger.info("Submissions list is: " + submissions);
 	Logger.info("Problems list is: " + problems);
         if(submissions.size()==0)
 	return ok(finalAssignment.render(problems,assignmentId, userId, getPrefix()));
@@ -142,6 +175,13 @@ public class HomeController extends Controller {
 		if (prefix == null) return ""; else return prefix;
 	}
 
+	public Result deleteProblem(Long assignmentId, Long problemID) {
+  		Problem.delete(problemID);
+		Assignment assignment = Assignment.find.byId(assignmentId);
+		List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).findList();
+  		return ok(editAssignment.render(assignment, problems));
+	}
+	
 	public Result saveEditedAssignment(Long assignment) {
         
 	      DynamicForm bindedForm = Form.form().bindFromRequest();
@@ -149,7 +189,7 @@ public class HomeController extends Controller {
 	      Assignment assignment1 = Assignment.find.byId(assignment);
               Logger.info(problemlist);
 		if(null != problemlist|| !problemlist.equals("")) {
-			String [] problemArr = problemlist.split(","); 
+			String [] problemArr = problemlist.split("\n"); 
 			for(String problemstr : problemArr) {
 			    if(null != problemstr && !problemstr.equals("")) {
 				Problem problem = new Problem();
@@ -161,7 +201,7 @@ public class HomeController extends Controller {
 			}
 		}
 	     List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment1.assignmentId).findList();
-    	System.out.println(problems);
+
 	    Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
 	    String returnUrl = launchReturnUrlCookie.value();
 	Logger.info("ReturnURL is: " + returnUrl);
