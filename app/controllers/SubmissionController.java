ea94db9f94898dd992359d213523a1fb50d3701c
Tyler Wasniowski <TylerWasniowski@gmail.com>
2017-07-07 21:33:13 -0700

    Removed redundent reloading of iframe that was nullifying the effect of codecheck state restoration

--- app/controllers/SubmissionController.java
+++ app/controllers/SubmissionController.java
@@ -1,99 +1,85 @@
 package controllers;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import play.*;
-import play.data.*;
-import static play.data.Form.*;
-import java.net.*;
-import java.io.*;
-import play.mvc.BodyParser;                     
-import play.libs.Json.*;   
-import play.libs.Jsonp;                     
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import static play.libs.Json.toJson;
+import models.Problem;
+import models.Submission;
 import play.Logger;
-import play.mvc.*;
-import models.*;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class SubmissionController extends Controller {
 
-   // Method to save the codecheck submission that is sent back from codecheck server
-   public Result addSubmission(Long problemID, Long assignmentID, String userID) {	
-	Logger.info("Result is received" );
+    // Method to save the codecheck submission that is sent back from codecheck server
+    public Result addSubmission(Long problemID, Long assignmentID, String userID) {
+        Logger.info("Result is received");
         JsonNode json = request().body().asJson();
-	if(json == null)
-		return badRequest("Expecting Json data");
+        if (json == null)
+            return badRequest("Expecting Json data");
 //        Logger.info("Received file is: " + json.toString());
-	String score = json.findPath("score").textValue();
+        String score = json.findPath("score").textValue();
 
-	Logger.info("Received score is:" + score);
-	String[] scores = score.split("/");
+        Logger.info("Received score is:" + score);
+        String[] scores = score.split("/");
 
-	Logger.info("AssignmentID is: " + assignmentID);
-	Logger.info("UserID is: " + userID);
+        Logger.info("AssignmentID is: " + assignmentID);
+        Logger.info("UserID is: " + userID);
 
-	Problem problem = Problem.find.byId(problemID);
-	Logger.info("Problem is: " + problem);
-		
-	List<Submission> submissions = Submission.find.where().eq("assignmentId",assignmentID).eq("studentId",userID).findList();
-	System.out.println(submissions);
-	
-	Submission submission = new Submission();
-	submission.setAssignmentId(assignmentID);
-	submission.setStudentId(userID);
+        Problem problem = Problem.find.byId(problemID);
+        Logger.info("Problem is: " + problem);
+
+        List<Submission> submissions = Submission.find.where().eq("assignmentId", assignmentID).eq("studentId", userID).findList();
+        System.out.println(submissions);
+
+        Submission submission = new Submission();
+        submission.setAssignmentId(assignmentID);
+        submission.setStudentId(userID);
         submission.setContent(json.toString());
-	submission.setCorrect(Long.parseLong(scores[0]));
-	if(scores.length >1)
-		submission.setMaxScore(Long.parseLong(scores[1]));
-	else
-		submission.setMaxScore(0L);
-	submission.setProblem(problem);
-	problem.getSubmissions().add(submission);
-	submission.save();
-	
-	System.out.println("New score is added and the value is: "+ score);
-        return ok("Score is saved");	
-     }
+        submission.setCorrect(Long.parseLong(scores[0]));
+        if (scores.length > 1)
+            submission.setMaxScore(Long.parseLong(scores[1]));
+        else
+            submission.setMaxScore(0L);
+        submission.setProblem(problem);
+        problem.getSubmissions().add(submission);
+        submission.save();
 
-	//Method to save interactive Exercise score
-	public Result addSubmissions(Long assignmentID){
-	   JsonNode jsonPayload = request().body().asJson();
-           Logger.info("json from client = {}", jsonPayload);
+        System.out.println("New score is added and the value is: " + score);
+        return ok("Score is saved");
+    }
 
-	   Logger.info("AssignmentID is: " + assignmentID);
-	   Http.Cookie userIdCookie = request().cookie("user_id");
-	   String userId = userIdCookie.value();
-	   Logger.info("UserID is: " + userId);
-           Iterator<JsonNode> nodeIterator = jsonPayload.elements();
-		
+    //Method to save interactive Exercise score
+    public Result addSubmissions(Long assignmentID) {
+        JsonNode jsonPayload = request().body().asJson();
+        Logger.info("json from client = {}", jsonPayload);
+
+        Logger.info("AssignmentID is: " + assignmentID);
+        Http.Cookie userIdCookie = request().cookie("user_id");
+        String userId = userIdCookie.value();
+        Logger.info("UserID is: " + userId);
+        Iterator<JsonNode> nodeIterator = jsonPayload.elements();
+
         while (nodeIterator.hasNext()) {
             JsonNode exercise = nodeIterator.next();
-	    if(exercise.has("activity")){
-	       Problem problem = Problem.find.where().eq("assignment.assignmentId",assignmentID).like("url", "%"+exercise.get("activity").asText()+"%").findList().get(0);
-	       List<Submission> submissions = Submission.find.where().eq("assignmentId",assignmentID).eq("studentId",userId).findList();
-	       Logger.info("Submission is: " + submissions);
+            if (exercise.has("activity")) {
+                Problem problem = Problem.find.where().eq("assignment.assignmentId", assignmentID).like("url", "%" + exercise.get("activity").asText() + "%").findList().get(0);
+                List<Submission> submissions = Submission.find.where().eq("assignmentId", assignmentID).eq("studentId", userId).findList();
+                Logger.info("Submission is: " + submissions);
 
-		   Submission submission = new Submission();
-	       submission.setAssignmentId(assignmentID);
-	       submission.setProblem(problem);
-	       submission.setStudentId(userId);
-	       submission.setActivity(exercise.get("activity").asText());
-	       submission.setCorrect(exercise.get("correct").asLong());
-	       submission.setMaxScore(exercise.get("maxscore").asLong());
-	       submission.save();
-	    
-           }
-	 }
-	return ok();
+                Submission submission = new Submission();
+                submission.setAssignmentId(assignmentID);
+                submission.setProblem(problem);
+                submission.setStudentId(userId);
+                submission.setActivity(exercise.get("activity").asText());
+                submission.setCorrect(exercise.get("correct").asLong());
+                submission.setMaxScore(exercise.get("maxscore").asLong());
+                submission.save();
+            }
+        }
+        return ok();
     }
 }
 
