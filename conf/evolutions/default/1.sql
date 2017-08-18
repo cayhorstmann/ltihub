07681913e79c06b364b792a5ea11cf995659309e
Cay Horstmann <432187+cayhorstmann@users.noreply.github.com>
2017-08-18 16:57:45 +0000

    Change to PostgreSQL

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table assignment (
-  assignment_id             bigint auto_increment not null,
+  assignment_id             bigserial not null,
   context_id                varchar(255),
   resource_link_id          varchar(255),
   tool_consumer_instance_gu_id varchar(255),
@@ -13,41 +13,37 @@ create table assignment (
 ;
 
 create table problem (
-  problem_id                bigint auto_increment not null,
+  problem_id                bigserial not null,
   url                       varchar(255),
   assignment_assignment_id  bigint,
   constraint pk_problem primary key (problem_id))
 ;
 
 create table submission (
-  submission_id             bigint auto_increment not null,
+  submission_id             bigserial not null,
   student_id                varchar(255),
   assignment_id             bigint,
   correct                   bigint,
   maxscore                  bigint,
   activity                  varchar(255),
-  submitted_at              datetime(6),
+  submitted_at              timestamp,
   content                   varchar(255),
   problem_problem_id        bigint,
   constraint pk_submission primary key (submission_id))
 ;
 
-alter table problem add constraint fk_problem_assignment_1 foreign key (assignment_assignment_id) references assignment (assignment_id) on delete restrict on update restrict;
+alter table problem add constraint fk_problem_assignment_1 foreign key (assignment_assignment_id) references assignment (assignment_id);
 create index ix_problem_assignment_1 on problem (assignment_assignment_id);
-alter table submission add constraint fk_submission_problem_2 foreign key (problem_problem_id) references problem (problem_id) on delete restrict on update restrict;
+alter table submission add constraint fk_submission_problem_2 foreign key (problem_problem_id) references problem (problem_id);
 create index ix_submission_problem_2 on submission (problem_problem_id);
 
 
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+drop table if exists assignment cascade;
 
-drop table assignment;
+drop table if exists problem cascade;
 
-drop table problem;
-
-drop table submission;
-
-SET FOREIGN_KEY_CHECKS=1;
+drop table if exists submission cascade;
 
