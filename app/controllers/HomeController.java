9a7c4325f8b8b8f857270cd2c05b68f59e4c7aab
Cay Horstmann <cay@horstmann.com>
2017-08-27 15:25:56 -0700

    create assignment wip

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import play.*;
@@ -23,92 +28,168 @@ import views.html.*;
 
 
 public class HomeController extends Controller {
+	private static String getParam(Map<String, String[]> params, String key) {
+		String[] values = params.get(key);
+		if (values == null || values.length == 0) return null;
+		else return values[0];
+	}
+	
+	private static boolean isInstructor(String role) {
+		return role.contains("Faculty")|| role.contains("TeachingAssistant") || role.contains("Instructor");
+	}
+	
+	private static boolean isEmpty(String str) {
+		return str == null || str.trim().length() == 0 || str.trim().equals("null");		
+	}
+	
+	private static String getStackTrace(Throwable t) {
+		StringWriter out = new StringWriter();
+		t.printStackTrace(new PrintWriter(out));
+		return out.toString();
+	}	
+
+	private static String httpPost(String urlString, Map<String, String> postData) {
+		StringBuilder result = new StringBuilder();
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			try (Writer out = new OutputStreamWriter(
+					connection.getOutputStream(), StandardCharsets.UTF_8)) {
+				boolean first = true;
+				for (Map.Entry<String, String> entry : postData.entrySet()) {
+					if (first) first = false;
+					else out.write("&");
+					out.write(URLEncoder.encode(entry.getKey(), "UTF-8"));
+					out.write("=");
+					out.write(URLEncoder.encode(entry.getValue(), "UTF-8"));
+				}
+			}
+			int response = connection.getResponseCode();
+			result.append(response);
+			result.append("\n");
+			try (Scanner in = new Scanner(connection.getInputStream(), "UTF-8")) {
+				while (in.hasNextLine()) {
+					result.append(in.nextLine());
+					result.append("\n");
+				}
+			}
+			catch (IOException e) {
+			    InputStream err = connection.getErrorStream();
+			    if (err == null) throw e;
+			    try (Scanner in = new Scanner(err, "UTF-8")) {
+			        result.append(in.nextLine());
+			        result.append("\n");
+			    }
+			}			
+		} catch (Throwable ex) {
+			result.append(getStackTrace(ex));
+		}
+		return result.toString();		
+	}
+
 
     public Result config() throws UnknownHostException {
         Http.Request request = request();
-        return ok(views.xml.lti_config.render())
-                .as("application/xml");
+        return ok(views.xml.lti_config.render()).as("application/xml");
     }     
     
-    public Result index() throws UnsupportedEncodingException{    
- 	Map<String, String[]> postParams = request().body().asFormUrlEncoded();
-    	for(String key: postParams.keySet())
-    		Logger.info(key + " - " + Arrays.toString(postParams.get(key)));
+    public Result index() throws UnsupportedEncodingException {    
+	 	Map<String, String[]> postParams = request().body().asFormUrlEncoded();
+	 	Logger.info("HomeController.index: ");
+    	for (String key : postParams.keySet())
+    		Logger.info(key + ": " + Arrays.toString(postParams.get(key)));
 
-	if (postParams.get("lis_outcome_service_url") == null || postParams.get("lis_result_sourcedid") == null) {
+    	String lisOutcomeServiceURL = getParam(postParams, "lis_outcome_service_url");
+    	String lisResultSourcedID = getParam(postParams, "lis_result_sourcedid");
+		if (lisOutcomeServiceURL == null || lisResultSourcedID == null) {
           	flash("warning", "");
-	}
-	else{	
-	response().setCookie(new Http.Cookie("lis_outcome_service_url", postParams.get("lis_outcome_service_url")[0],
-                 null, null, null, false, false));
-	response().setCookie(new Http.Cookie("lis_result_sourcedid", URLEncoder.encode(postParams.get("lis_result_sourcedid")[0],"UTF-8"),
-                 null, null, null, false, false));
-	}
-	if(postParams.get("custom_canvas_user_id")==null){
-		String userid = postParams.get("user_id")[0];
-		Logger.info("User ID from ilearn is: " + userid);
-		response().setCookie(new Http.Cookie("user_id", postParams.get("user_id")[0],null, null, null, false, false));
-	}else
-		response().setCookie(new Http.Cookie("user_id", postParams.get("custom_canvas_user_id")[0],null, null, null, false, false));
-	response().setCookie(new Http.Cookie("launch_presentation_return_url", postParams.get("launch_presentation_return_url")[0],
-                    null, null, null, false, false));
-	String contextID = postParams.get("context_id")[0];
-	String resourceLinkID = postParams.get("resource_link_id")[0];
-	String toolConsumerInstanceGuID = postParams.get("tool_consumer_instance_guid")[0];
-	String role = postParams.get("roles")[0];
-	Logger.info("Role is: " + role);
-        String assignmentId = request().getQueryString("id");
-	Logger.info("Assignment id is: "+ assignmentId);
-	if(assignmentId == null && (role.contains("Faculty")|| role.contains("TeachingAssistant") || role.contains("Instructor")))
-		return ok(create_exercise.render(contextID, resourceLinkID, toolConsumerInstanceGuID));
+		}
+		else { // TODO: No cookies	
+			response().setCookie(new Http.Cookie("lis_outcome_service_url", lisOutcomeServiceURL,
+		                 null, null, null, false, false));
+			response().setCookie(new Http.Cookie("lis_result_sourcedid", URLEncoder.encode(lisResultSourcedID,"UTF-8"),
+		                 null, null, null, false, false));
+		}
+		String userID = getParam(postParams, "custom_canvas_user_id"); 
+		if (userID == null) userID = getParam(postParams, "user_id");
+		Logger.info("User ID: " + userID);
+		response().setCookie(new Http.Cookie("user_id", userID, null, null, null, false, false));  // TODO: No cookies
 
-	return redirect(controllers.routes.HomeController.getAssignment(role, Long.parseLong(assignmentId)));	
-     	
+		// response().setCookie(new Http.Cookie("launch_presentation_return_url", postParams.get("launch_presentation_return_url")[0],
+	    //                null, null, null, false, false));
+		String contextID = getParam(postParams, "context_id");
+		String resourceLinkID = getParam(postParams, "resource_link_id");
+		String toolConsumerInstanceGuID = getParam(postParams, "tool_consumer_instance_guid");
+		String role = getParam(postParams, "roles");
+		String launchPresentationReturnURL = getParam(postParams, "launch_presentation_return_url");
+	    String assignmentId = request().getQueryString("id");
+		
+		if (assignmentId == null && isInstructor(role))
+			return ok(create_exercise.render(contextID, resourceLinkID, toolConsumerInstanceGuID, launchPresentationReturnURL));
+		else
+			return redirect(controllers.routes.HomeController.getAssignment(role, Long.parseLong(assignmentId)));		     	
  	}
 
-	public Result createAssignment() {
-		
+	public Result createAssignment() {		
          return ok(create_exercise_outside_LMS.render());
     }
 		
-	//Method to save the created assignment
+	// This method gets called when an assignment has been created with create_exercise.scala.html.
 	public Result addAssignment() {        
-	       DynamicForm bindedForm = Form.form().bindFromRequest();
-   	       String problemlist = bindedForm.get("url");
-	       
-               Assignment assignment = new Assignment();
-	       assignment.setContextId(bindedForm.get("context_id"));
-	       assignment.setResourceLinkId(bindedForm.get("resource_link_id"));
-	       assignment.setToolConsumerInstanceGuId(bindedForm.get("tool_consumer_id"));
-	       
-	       String duration = bindedForm.get("duration");
-	       if(duration.equals(""))
-			assignment.setDuration(0L);
-	       else
-			assignment.setDuration(Long.parseLong(duration));
-               assignment.save();
- 	       Logger.info(problemlist);
-	   
-               if(null != problemlist|| !problemlist.equals("")) {
-		      String [] problemArr = problemlist.split("\n"); 
-		      for(String problemstr : problemArr) {
-			  if(null != problemstr && !problemstr.equals("")) {
-				  Problem problem = new Problem();
-				  problem.setProblemUrl(problemstr);
-				  problem.setAssignment(assignment);
-				  assignment.getProblems().add(problem);
-				  problem.save();
-			  }	
-		     }
-	     }
-	     List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).orderBy("problemId").findList();
+       DynamicForm bindedForm = Form.form().bindFromRequest();
+       String problemlist = bindedForm.get("url");
+       
+       Assignment assignment = new Assignment();
+       assignment.setContextId(bindedForm.get("context_id"));
+       assignment.setResourceLinkId(bindedForm.get("resource_link_id"));
+       assignment.setToolConsumerInstanceGuId(bindedForm.get("tool_consumer_id"));
+       
+       String duration = bindedForm.get("duration");
+       if(duration.equals(""))
+    	   assignment.setDuration(0L);
+       else
+    	   assignment.setDuration(Long.parseLong(duration));
+       assignment.save();
+       Logger.info(problemlist);
+   
+       if (!isEmpty(problemlist)) {
+		   for (String problemstr : problemlist.split("\\s+")) {
+			   if (!isEmpty(problemstr)) {
+				   Problem problem = new Problem();
+				   problem.setProblemUrl(problemstr);
+				   problem.setAssignment(assignment);
+				   assignment.getProblems().add(problem);
+				   problem.save();
+			   }	
+		   }
+       }
+       /*
+        Old approach: make form that posts to the launch_presentation_return_url
+         
+       List<Problem> problems = Problem.find.fetch("assignment").where().eq("assignment.assignmentId",assignment.assignmentId).orderBy("problemId").findList();
 	
 	     Http.Cookie launchReturnUrlCookie = request().cookie("launch_presentation_return_url");
 	     String returnUrl = launchReturnUrlCookie.value();
 	
 	     return ok(showassignment.render(returnUrl,assignment,problems, getPrefix()));
-
-       }
+		*/
+       
+       // POST to the launch_presentation_return_url
+       String launchPresentationReturnURL = bindedForm.get("launch_presentation_return_url");
+       if (!isEmpty(launchPresentationReturnURL)) {
+    	   Map<String, String> postParams = new LinkedHashMap<>();
+    	   
+    	   String assignmentURL = (request().secure() ? "https://" : "http://" ) 
+    			   + request().host() + getPrefix() + "/assignment?id=" + assignment.getAssignmentId();
+    	   postParams.put("url", assignmentURL);
+    	   String outcome = httpPost(launchPresentationReturnURL, postParams);
+    	   Logger.info("POST " + assignmentURL + " " + postParams + " " + outcome);
+    	   return ok(assignmentURL);
+       } else {
+    	   return ok("No launch presentation return URL");
+       }       
+    }
 	
 	public Result addAssignmentOutsideLMS() {        
 	      DynamicForm bindedForm = Form.form().bindFromRequest();
@@ -212,8 +293,12 @@ public class HomeController extends Controller {
 
 	public String getPrefix() { 
 		String prefix = System.getProperty("play.http.context");
-		Logger.info("Prefix is: " + prefix);
-		if (prefix == null) return ""; else return prefix;
+		if (prefix == null) return "/"; 
+		else {
+			if (prefix.endsWith("/")) prefix = prefix.substring(0, prefix.length() - 1);
+			if (!prefix.startsWith("/")) prefix = "/" + prefix;
+			return prefix;
+		}
 	}
 
 	public Result deleteProblem(Long assignmentId, Long problemID) {
