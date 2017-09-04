3b4fc47eb52a336fb70b9b33209b38b1c645f505
Tyler Wasniowski <TylerWasniowski@gmail.com>
2017-09-03 23:04:41 -0700

    Added DataProvier class to prepare for submissions viewer

---
+++ app/services/DataProvider.java
@@ -0,0 +1,67 @@
+package services;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Assignment;
+import models.Problem;
+import models.Submission;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.Result;
+
+import java.util.*;
+
+import static play.mvc.Results.badRequest;
+import static play.mvc.Results.ok;
+
+/**
+ * Has methods meant to provide data to pages that request it
+ */
+public class DataProvider {
+
+    /**
+     * Provides the user ids of the users that have made submissions to any of the problems on the given assignment
+     * @param assignmentId the id of the assignment to collect the user ids of
+     * @return a stringified json object with the user ids of the users that have made submissions
+     * to any of the problems on the given assignment
+     */
+    public static Result getUserIdsForAssignment(Long assignmentId) {
+        Assignment assignment = Assignment.find.byId(assignmentId);
+        if (assignment == null)
+            return badRequest("Assignment not found. ID Given: " + assignmentId);
+
+        Set<String> userIds = new TreeSet<>();
+        for (Problem problem: assignment.getProblems()) {
+            for (Submission submission: problem.getSubmissions()) {
+                userIds.add(submission.getStudentId());
+            }
+        }
+
+        JsonNode userIdsJSON = Json.toJson(userIds);
+
+        return ok(Json.stringify(userIdsJSON));
+    }
+
+    /**
+     * Provides contents from all of the submissions for a given problem id and user id
+     * @param problemId the id of the problem to find the submissions for
+     * @param studentId the id of the user to find the submissions for
+     * @return a stringified json object with contents from all of the submissions for a given problem id and user id
+     */
+    public static Result getSubmissionContent(Long problemId, String studentId) {
+        Problem problem = Problem.find.byId(problemId);
+        if (problem == null)
+            return badRequest("Problem not found. ID Given: " + problemId);
+
+        Collection<String> contents = new LinkedList<>();
+        for (Submission submission: problem.getSubmissions()) {
+            if (studentId.equals(submission.getStudentId())) {
+                contents.add(submission.getContent());
+            }
+        }
+
+        JsonNode contentsJSON = Json.toJson(contents);
+
+        return ok(Json.stringify(contentsJSON));
+    }
+}
