e101b940db81d6f19c6f76d7e48e1775090f7e8e
Cay Horstmann <cay@horstmann.com>
2018-01-28 20:23:22 -0800

    ...

--- app/models/Util.java
+++ app/models/Util.java
@@ -222,7 +222,8 @@ public class Util {
 	}
 	
 	public static String getSharedSecret(String oauthConsumerKey) {
-		Oauth oauth = Ebean.find(Oauth.class, oauthConsumerKey);
+		Oauth oauth = Ebean.find(Oauth.class)
+				.where().eq("oauthConsumerKey", oauthConsumerKey).findOne();
 		String sharedSecret = "";
 		if (oauth != null) sharedSecret = oauth.sharedSecret;
 		Logger.info("shared secret for " + oauthConsumerKey + " is " + sharedSecret);
